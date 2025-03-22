f0c89756391eb9d44624c5a429cb3d7a1d3108de
DavideM10 <davidemiotti30@gmail.com>
2025-03-22 14:05:01 +0000

    Primo commit del progetto

---
+++ index.js
@@ -0,0 +1,9 @@
+// Gestisci il click sui pulsanti per accedere ai giochi
+document.getElementById('game1').addEventListener('click', function() {
+    window.location.href = 'game1.html'; // Redirige al gioco 1
+  });
+  
+  document.getElementById('game2').addEventListener('click', function() {
+    window.location.href = 'game2.html'; // Redirige al gioco 2 (simulando Blockbuster)
+  });
+  
